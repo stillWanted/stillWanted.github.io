ae297671a102b2ba6e6e7e33cac6d6075ecf7c13
stillWanted <36459879+stillWanted@users.noreply.github.com>
2020-04-29 03:23:48 +0200

    Update script.js

--- test/script.js
+++ test/script.js
@@ -2,20 +2,7 @@ dragula([
   document.getElementById("to-do"),
   document.getElementById("doing"),
   document.getElementById("done"),
-  document.getElementById("trash")
-]).on("drag", function(el) {
-    el.className.replace("ex-moved", "");
-  })
-  .on("drop", function(el) {
-    el.className += "ex-moved";
-  })
-  .on("over", function(el, container) {
-    container.className += "ex-over";
-  })
-  .on("out", function(el, container) {
-    container.className.replace("ex-over", "");
-  });
-
+  document.getElementById("trash")]);
 
 function addTask(url) {
   document.getElementById("to-do").innerHTML +=
